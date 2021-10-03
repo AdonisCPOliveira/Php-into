e9982595fab4e359c36b1a2e0c2e75adbce3f04c
AdonisCPOliveira <estudo.adonis@gmail.com>
2021-10-03 16:44:13 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+$categorias = [];
+$categorias[0] = "infantil";
+$categorias[1] = "adolescente";
+$categorias[2] = "adulto";
+
+$nome = "Adonis";
+$idade = 33;
+
+if ($idade >= 6 && $idade <= 12) {
+    for ($i = 0; $i < count($categorias); $i++) {
+        if ($categorias[$i] == "infantil") {
+            echo "O nadador " . $nome . " compete na categoria " . $categorias[$i] . "!";
+        }
+    }
+} else if ($idade >= 13 && $idade <= 18) {
+    for ($i = 0; $i < count($categorias); $i++) {
+        if ($categorias[$i] == "adolescente") {
+            echo "O nadador " . $nome . " compete na categoria " . $categorias[$i] . "!";
+        }
+    }
+} else {
+    for ($i = 0; $i < count($categorias); $i++) {
+        if ($categorias[$i] == "adulto") {
+            echo "O nadador " . $nome . " compete na categoria " . $categorias[$i] . "!";
+        }
+    }
+}
+
+
